87b28c960adede9c05bbe4e7b2071fb65831c214
JessicaQueiroz <jehqueiroz01@gmail.com>
2020-09-30 19:05:18 -0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String nome = "Variáveis";
+	    int numero = 10;
+	    int numero2 = 5;
+	    double altura = 1.65;
+	    int soma = (numero + numero2);
+
+	    System.out.printf("Nome: %s, Soma: %d, Altura: %.2f", nome, soma, altura);
+
+    }
+}
